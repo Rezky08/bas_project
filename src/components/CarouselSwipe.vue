90e0e5d65d25ef596d638ccc7cbd47d9e6839e61
rezky setiawan <rezky.setiawan85@gmail.com>
2020-10-20 08:25:35 +0700

    custom swipe carousel

---
+++ src/components/CarouselSwipe.vue
@@ -0,0 +1,29 @@
+<template>
+<div id="carousel" v-touch="{
+    left: () => model--,
+    right: () => model++,
+}">
+    <v-carousel v-model="model" next-icon="mdi-chevron-right" prev-icon="mdi-chevron-left" delimiter-icon="mdi-dot" hide-delimiter-background :show-arrows='!isMobile' :cycle='true' interval="4000">
+        <slot name="items">
+        </slot>
+
+    </v-carousel>
+</div>
+</template>
+
+<script>
+export default {
+    name: 'carouselswipe',
+    data() {
+        return {
+            model: 0
+        }
+    },
+    props: {
+        isMobile: {
+            type: Boolean,
+            default: false
+        }
+    }
+}
+</script>
