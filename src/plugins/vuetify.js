ff4d704d51ae64e24e03fa845a3fae9bca41343f
rezky setiawan <rezky.setiawan85@gmail.com>
2020-10-20 08:26:14 +0700

    system

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,9 +1,11 @@
 import Vue from "vue";
 import Vuetify from "vuetify/lib";
+import '@mdi/font/css/materialdesignicons.css'
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
+  icons:'mdi',
   theme: {
     themes: {
       light: {
